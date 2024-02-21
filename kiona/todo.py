a84db3a6d48d728bf9dbeb8bbbcdb3994a340ca9
kionahz <khitz.pi@gmail.com>
2024-02-21 14:35:46 +0100

    todo list

---
+++ kiona/todo.py
@@ -0,0 +1,29 @@
+
+'''
+helpers file: background loading
+
+each page own python file
+
+main page app is the start page
+todo bg map image
+todo text on each page
+
+todo sidebar (calls each page):
+Shelly (maybe in Speech bubble actions)
+
+Actions:
+- method 1
+    quest 1
+    quest 2
+    quest 3
+
+-method 2
+    quest 1
+    quest 2
+    quest 3
+
+-home
+-backpack
+
+
+'''

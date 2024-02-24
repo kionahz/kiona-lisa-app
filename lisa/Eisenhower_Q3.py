8040f959b312f36a27b72c0c46d32adc4344c384
limarly <lisa.m.schmidt@stud.leuphana.de>
2024-02-24 12:43:25 +0100

    commit update

--- lisa/Eisenhower_Q3.py
+++ lisa/Eisenhower_Q3.py
@@ -7,17 +7,21 @@ def Eisenhower_quest3():
              "Watch the Sunset", "Search for drinking water", "Search for Food", "Build a Weapon",
              "Explore the surroundings"]
 
-    st.title("Now it's your turn!")
-    col1, col2 = st.columns(2)
+    col_title, col_img = st.columns((2,1))
 
-    with col1:
+    with col_title:
+        st.title("Now it's your turn!")
+        st.markdown(
+            f""" <p style="line-height:130%; font-size: 1.5vw; color: white">Now you know everything about the 
+            Eisenhower matrix. Let's try to arrange your tasks on what you can do to escape the island once again. Do 
+            you think you can do it like Eisenhower? Select the task you would like to sort into your very own 
+            Eisenhower matrix with with the multiselects.</p>""",
+            unsafe_allow_html=True
+        )
+
+    with col_img:
         st.image("../pictures/em_empty.png")
 
-    with col2:
-        st.markdown(
-            "Now you know everything about the Eisenhower matrix. Let's try to arrange what you can do to escape the "
-            "island once again. Do you think you can do it like Eisenhower? Select the task you would like to sort "
-            "into your very own Eisenhower matrix with with the multiselects.")
 
     sel_urgent_important, sel_noturgent_important = st.columns(2)
     sel_urgent_notimportant, sel_noturgent_notimportant = st.columns(2)
@@ -31,22 +35,22 @@ def Eisenhower_quest3():
 
 
     with sel_noturgent_important:
+        st.subheader("Not Urgent/Important")
         selections_noturgent_important = st.multiselect("Not Urgent/Important", tasks, label_visibility="hidden")
         st.write(selections_noturgent_important)
 
 
     with sel_urgent_notimportant:
+        st.subheader("Urgent/Not Important")
         selections_urgent_notimportant = st.multiselect(
-            "Urgent/Not Important", tasks
-        )
-
+            "Urgent/Not Important", tasks, label_visibility="hidden")
+        st.write(selections_urgent_notimportant)
 
     with sel_noturgent_notimportant:
+        st.subheader("Not Urgent/Not Important")
         selections_noturgent_notimportant = st.multiselect(
-            "Not Urgent/Not Important", tasks
-        )
-    with tab_noturgent_notimportant:
-        st.markdown("Not Urgent/Not Important")
-        st.markdown(selections_noturgent_notimportant)
+            "Not Urgent/Not Important", tasks, label_visibility="hidden")
+        st.write(selections_noturgent_notimportant)
+
 
 

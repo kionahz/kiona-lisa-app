305b667f6cf6a2058cab76d2efd45a3a5be91495
kionahz <khitz.pi@gmail.com>
2024-02-28 11:59:46 +0100

    shelly_captain and sail away layout

--- kiona/src/sail_away.py
+++ kiona/src/sail_away.py
@@ -20,14 +20,14 @@ def autoplay_audio(file_path: str):
 
 
 def render_sail_away():
-    col1, col2 = st.columns((1, 5))
-    col1.image("../pictures/shelly.png")
-    col2.title("Well done Sailor! You can now call yourself CAPTAIN PRODUCTIVITY!!! Sail home safely!")
-
-    col3, col4, col5 = st.columns((1, 5, 2))
-    with col4:
+    col1, col2 = st.columns((5, 3), gap="large")
+    with col1:
         st.image("../pictures/ship.png")
-    with col5:
+    with col2:
+        st.markdown(
+            f"""<p style="line-height:130%; font-size: 1.5vw; color: white">Well done Sailor! You can now call 
+            yourself CAPTAIN PRODUCTIVITY!!!<br><br>Sail home safely!</p> """, unsafe_allow_html=True)
+        st.image("../pictures/shelly_captain.png", width=300)
         autoplay_audio("../audio/Orinoco-Flow.mp3")
 
     cola, colb = st.columns((8, 1))
